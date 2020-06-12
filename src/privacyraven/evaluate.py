afa64f7b9241734721d61fa4284af442bdaafe6e
suhacker1 <suhashussain1@gmail.com>
2020-06-12 13:27:37 -0400

    Add transfer learning model; separate attack actions

---
+++ src/privacyraven/evaluate.py
@@ -0,0 +1,36 @@
+import torch
+import tqdm
+
+from privacyraven.query import get_query_target
+
+
+def label_agreement(
+    test,
+    substitute_model,
+    victim_model,
+    query=get_query_target,
+    substitute_size=None,
+    victim_size=None,
+    points=100,
+):
+    agreed = 0
+
+    for i in tqdm(range(1, points)):
+        # Case: Torchvision Dataset
+        x, y = test[i]
+
+        substitute_result = int(query(substitute_model, x, substitute_size))
+        victim_result = int(query(victim_model, x, victim_size))
+
+        if substitute_result == victim_result:
+            agreed = agreed + 1
+
+    print(
+        "Out of "
+        + str(points)
+        + " data points, the models agreed upon "
+        + str(agreed)
+        + "."
+    )
+
+    return agreed
--- src/privacyraven/models.py
+++ src/privacyraven/models.py
@@ -5,6 +5,7 @@ import pytorch_lightning as pl
 import torch
 from torch import nn
 from torch.nn import functional as F
+from torchvision import datasets, models, transforms
 from tqdm import tqdm
 
 
@@ -116,6 +117,139 @@ class ThreeLayerClassifier(pl.LightningModule):
             outputs (array): result of test step for each batch
         Returns:
             Formatted string with resultant metrics"""
+        avg_loss = torch.stack([x["test_loss"] for x in outputs]).mean()
+        tensorboard_logs = {"test_loss": avg_loss}
+        return {"avg_test_loss": avg_loss, "log": tensorboard_logs}
+
+
+class ImagenetTransferLearning(pl.LightningModule):
+    def __init__(self, hparams):
+        """Create a classifier with a pretrained MobileNet backbone"""
+        super(ImagenetTransferLearning, self).__init__()
+        self.hparams = hparams
+        self.feature_extractor = models.mobilenet_v2(pretrained=True)
+        self.feature_extractor.eval()
+
+        # Establish classifier
+        self.layer_1 = torch.nn.Linear(hparams["input_size"], 128)
+        self.layer_2 = torch.nn.Linear(128, 256)
+        self.layer_3 = torch.nn.Linear(256, hparams["targets"])
+
+    def forward(self, x):
+        """Establishes the neural network's forward pass
+
+        Parameters:
+            x (Torch tensor): input image
+
+        Returns:
+            Output probability vector for classes
+        """
+        x = self.feature_extractor(x)
+        batch_size, hidden = x.size()
+
+        x = self.layer_1(x)
+        x = torch.relu(x)
+        x = self.layer_2(x)
+        x = torch.relu(x)
+        x = self.layer_3(x)
+
+        x = torch.log_softmax(x, dim=1)
+        return x
+
+    def nll_loss(self, logits, labels):
+        """Calculates loss
+
+        Parameters:
+            logits (Torch tensor): model output predictions
+            labels (Torch tensor): true values for predictions
+
+        Returns:
+            Loss
+        """
+        return F.nll_loss(logits, labels)
+
+    def training_step(self, train_batch, batch_idx):
+        """Pushes training data batch through model and calculates loss in loop
+
+        Parameters:
+            train_batch (Torch tensor): batch of training data from training dataloader
+            batch_idx (int): index of batch in contention
+
+        Returns:
+            Formatted string with cross entropy loss and training logs
+        """
+        x, y = train_batch
+        logits = self.forward(x)
+        loss = self.nll_loss(logits, y)
+        logs = {"train_loss": loss}
+        return {"loss": loss, "log": logs}
+
+    def validation_step(self, val_batch, batch_idx):
+        """Pushes validation data batch through model and calculates loss in loop
+
+        Parameters:
+            val_batch (Tensor): batch of validation data from validation dataloader
+            batch_idx (int): index of batch in contention
+
+        Returns:
+            Formatted string with resultant cross entropy loss
+        """
+        x, y = val_batch
+        logits = self.forward(x)
+        loss = self.nll_loss(logits, y)
+        targets_hat = torch.argmax(logits, dim=1)
+        n_correct_pred = torch.sum(y == targets_hat).item()
+        return {"val_loss": loss, "n_correct_pred": n_correct_pred, "n_pred": len(x)}
+
+    def validation_epoch_end(self, outputs):
+        """Returns validation step results at the end of the epoch
+
+        Parameters:
+            outputs (array): result of validation step for each batch
+
+        Returns:
+            Formatted string with resultant metrics
+        """
+        avg_loss = torch.stack([x["val_loss"] for x in outputs]).mean()
+        tensorboard_logs = {"val_loss": avg_loss}
+        return {"avg_val_loss": avg_loss, "log": tensorboard_logs}
+
+    def configure_optimizers(self):
+        """Sets up the optimization scheme"""
+        optimizer = torch.optim.Adam(
+            self.parameters(), lr=self.hparams["learning_rate"]
+        )
+        return optimizer
+
+    def test_step(self, batch, batch_idx):
+        """Pushes test data into the model and returns relevant metrics
+
+        Parameters:
+            batch (Torch tensor): batch of test data from test dataloader
+            batch_idx (int): index of batch in contention
+
+        Returns:
+            Formatted string with relevant metrics
+        """
+        x, y = batch
+        y_hat = self(x)
+        targets_hat = torch.argmax(y_hat, dim=1)
+        n_correct_pred = torch.sum(y == targets_hat).item()
+        return {
+            "test_loss": F.nll_loss(y_hat, y),
+            "n_correct_pred": n_correct_pred,
+            "n_pred": len(x),
+        }
+
+    def test_epoch_end(self, outputs):
+        """Returns test step results at the end of the epoch
+
+        Parameters:
+            outputs (array): result of test step for each batch
+
+        Returns:
+            Formatted string with resultant metrics
+        """
         avg_loss = torch.stack([x["test_loss"] for x in outputs]).mean()
         tensorboard_logs = {"test_loss": avg_loss}
         return {"avg_test_loss": avg_loss, "log": tensorboard_logs}
