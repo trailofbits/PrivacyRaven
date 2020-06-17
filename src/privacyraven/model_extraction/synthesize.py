28b7e0ef9f8daca77fcec52663fe766c0b70fa40
suhacker1 <suhashussain1@gmail.com>
2020-06-17 14:43:20 -0400

    Create seprarate folders for attacks; clarify synthesizer

--- src/privacyraven/model_extraction/synthesize.py
+++ src/privacyraven/model_extraction/synthesize.py
@@ -3,15 +3,15 @@ from tqdm import tqdm
 
 
 # Need to refactor this to facilitate different types of attacks
-def knockoff(data, query, model, points, victim_input_size, substitute_input_size):
-    for i in tqdm(range(0, points)):
+def knockoff(data, query, query_limit, victim_input_size, substitute_input_size):
+    for i in tqdm(range(0, query_limit)):
         if i == 0:
             x, y0 = data[0]
-            y = torch.tensor([query(model, x, victim_input_size)])
+            y = torch.tensor([query(x, victim_input_size)])
             x = x.reshape(substitute_input_size)
         else:
             xi, y0 = data[i]
-            yi = torch.tensor([query(model, xi, victim_input_size)])
+            yi = torch.tensor([query(xi, victim_input_size)])
             xi = xi.reshape(substitute_input_size)
             x = torch.cat((x, xi))
             y = torch.cat((y, yi))
