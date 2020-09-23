f3a13e1cec4b87768b8cd105817b3e949b989939
suhacker <suhashussain1@gmail.com>
2020-09-23 15:49:17 -0400

    Reconfigure nox

--- noxfile.py
+++ noxfile.py
@@ -16,7 +16,7 @@ def tests(session):
 @nox.session(python=["3.8", "3.7"])
 def lint(session):
     args = session.posargs or locations
-    session.install("flake8", "flake8-bugbear", "flake8-import-order")
+    session.install("flake8", "flake8-bugbear", "flake8-bandit", "flake8-import-order")
     session.run("flake8", *args)
 
 
@@ -41,77 +41,5 @@ def safety(session):
             external=True,
         )
         session.install("safety")
-        session.run("safety", "check", f"--file={requirements.name}", "--full-report")
-
-
-
-locations = "src", "tests", "noxfile.py"
-nox.options.sessions = "lint", "safety", "tests"
-
-
-def install_with_constraints(session, *args, **kwargs):
-    #Install packages constrained by the Poetry lock file
-    with tempfile.NamedTemporaryFile() as requirements:
-        session.run(
-            "poetry",
-            "export",
-            "--dev",
-            "--format=requirements.txt",
-            f"--output={requirements.name}",
-            external=True,
-        )
-        session.install(f"--constraint={requirements.name}", *args, **kwargs)
-
-
-@nox.session(python=["3.8", "3.7"])
-def tests(session):
-    #Run the test suite
-    args = session.posargs or ["--cov", "-m", "not e2e"]
-    session.run("poetry", "install", "--no-dev", external=True)
-    install_with_constraints(
-        session, "coverage[toml]", "pytest", "pytest-cov", "pytest-mock"
-    )
-    session.run("pytest", *args)
-    #session.run("poetry run pytest", *args)
-
-
-@nox.session(python="3.8")
-def black(session):
-    #Format code with black#
-    args = session.posargs or locations
-    install_with_constraints(session, "black")
-    session.run("black", *args)
-
-
-@nox.session(python=["3.8", "3.7"])
-def lint(session):
-    #Lint with flake8
-    args = session.posargs or locations
-    install_with_constraints(
-        session,
-        "flake8",
-        "flake8-bandit",
-        "flake8-black",
-        "flake8-bugbear",
-        # "flake8-docstrings",
-        "flake8-import-order",
-    )
-    session.run("flake8", *args)
-
-
-@nox.session(python="3.8")
-def safety(session):
-    #Analyze dependencies for insecure packages
-    with tempfile.NamedTemporaryFile() as requirements:
-        session.run(
-            "poetry",
-            "export",
-            "--dev",
-            "--format=requirements.txt",
-            "--without-hashes",
-            f"--output={requirements.name}",
-            external=True,
-        )
-        install_with_constraints(session, "safety")
         session.run("safety", "check", f"--file={requirements.name}", "--full-report")
 """
