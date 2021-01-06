a9ecf01ae70109d39def9c8262de033adb7f04ce
Philip Wang <powdercheeze@gmail.com>
2021-01-05 20:27:04 -0500

    Update all examples

--- examples/create_synthesizer.py
+++ examples/create_synthesizer.py
@@ -1,29 +1,15 @@
 import privacyraven as pr
-from privacyraven.extraction.synthesis import (synthesize, register_synth)
+from privacyraven.extraction.synthesis import register_synth
 from privacyraven.utils.data import get_emnist_data
 from privacyraven.extraction.core import ModelExtractionAttack
 from privacyraven.utils.query import get_target
 from privacyraven.utils.query import reshape_input
 from privacyraven.models.victim import train_four_layer_mnist_victim
 from privacyraven.models.four_layer import FourLayerClassifier
-# Users may define a function to generate synthetic data via a function of the form
-#
-#   func(seed_data_train, query, query_limit, *args, **kwargs)
-#
-# This function then can be registered via the @register_synth decorator
-# See the following example (may also be found in synthesis.py): 
 
-@register_synth
-def custom_synthesizer(data, query, query_limit, victim_input_shape, substitute_input_shape, reshape=True):
-    """Creates a synthetic dataset by labeling seed data
 
-    Arxiv Paper: https://ieeexplore.ieee.org/document/8489592"""
-    (x_data, y_data) = data
-    #print("y_data: ", y_data)
-    y_data = query(x_data)
-    if reshape:
-        x_data = reshape_input(x_data, substitute_input_shape)
-    return x_data, y_data
+# Create a query function for a target PyTorch Lightning model
+model = train_four_layer_mnist_victim(gpus=1)
 
 # User-defined query function
 def query_mnist(input_data):
@@ -32,8 +18,23 @@ def query_mnist(input_data):
 # Obtain seed (or public) data to be used in extraction
 emnist_train, emnist_test = get_emnist_data()
 
-# Create a query function for a target PyTorch Lightning model
-model = train_four_layer_mnist_victim(gpus=1)
+
+# Users may define a function to generate synthetic data via a function of the form
+#
+#   func(seed_data_train, query, query_limit, *args, **kwargs)
+#
+# This function then can be registered via the @register_synth decorator
+# See the following example, which is an aliased version of the copycat synthesizer
+# that may be found in privacyraven.extraction.synthesis: 
+
+@register_synth
+def custom_synthesizer(data, query, query_limit, victim_input_shape, substitute_input_shape, reshape=True):
+    """Creates a synthetic dataset by labeling seed data"""
+    (x_data, y_data) = data
+    y_data = query(x_data)
+    if reshape:
+        x_data = reshape_input(x_data, substitute_input_shape)
+    return x_data, y_data
 
 # Gets name of synthesizer function.
 attack = ModelExtractionAttack(
--- examples/example_pytorch_callback.py
+++ examples/example_pytorch_callback.py
@@ -1,7 +1,12 @@
 """
-PrivacyRaven has support for user-defined callbacks, 
-which may be passed as arguments into the currently available attack functions.
+PrivacyRaven has support for user-defined Pytorch Lightning Bolt callbacks which may be passed as arguments 
+into the currently available attack functions.
 
+A callback is just a function (or class in this case) that gets passed in as an argument to another
+function, which should execute the callback function or class at some point during its runtime. 
+
+Users should refer to https://pytorch-lightning-bolts.readthedocs.io/en/latest/callbacks.html to construct 
+Pytorch Lightning Bolt callbacks.
 """
 import privacyraven as pr
 from privacyraven.utils.data import get_emnist_data
@@ -12,26 +17,27 @@ from privacyraven.models.victim import train_four_layer_mnist_victim
 from privacyraven.models.four_layer import FourLayerClassifier
 from pytorch_lightning.callbacks import Callback
 
-emnist_train, emnist_test = get_emnist_data()
 
-
-# Create a query function for a target PyTorch Lightning model
+# Trains victim model
 model = train_four_layer_mnist_victim(gpus=1)
 
+# Create a query function for a target PyTorch Lightning model
 def query_mnist(input_data):
     # PrivacyRaven provides built-in query functions
     return get_target(model, input_data, (1, 28, 28, 1))
 
+emnist_train, emnist_test = get_emnist_data()
 
-# User-defined callback that inherits from the Pytorch's Callback class
+# Below is a user-defined callback that inherits from the Pytorch's Lightning Bolt Callback class.
+# All it does is print "End of epoch" at the end of a training epoch.
 
 class CustomCallback(Callback):
     def on_epoch_end(self, trainer, pl_module):
         print('End of epoch')
 
 # Runs a Model Extraction Attack with the user-defined CustomCallback specified as an argument.
-# See https://pytorch-lightning-bolts.readthedocs.io/en/latest/callbacks.html for all valid Pytorch
-# Lightning callback hooks. Note that parentheses are needed while passing in the callback.
+# Note that parentheses are needed while passing in the callback, since 
+# Pytorch Lightning bolt callbacks are classes that need to be instantiated.
 
 attack = ModelExtractionAttack(
     query=query_mnist,
@@ -48,7 +54,7 @@ attack = ModelExtractionAttack(
     callback=CustomCallback()
 )
 
-# Many built-in Pytorch callbacks are already very useful.  Consider the following example, which 
+# Many built-in Pytorch Lightning Bolt callbacks are already very useful.  Consider the following example, which 
 # runs the same Model Extraction Attack with the Pytorch built-in PrintTableMetricsCallback specified as an argument.
 # After every epoch, a table should be displayed with all of the training metrics (e.g. training loss)
 attack = ModelExtractionAttack(
