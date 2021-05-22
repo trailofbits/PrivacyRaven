82655a6f18658e39f5945b90d64a763a66beffc1
Philip Wang <powdercheeze@gmail.com>
2021-05-22 19:11:10 -0400

    Update inversion

--- src/privacyraven/inversion/core.py
+++ src/privacyraven/inversion/core.py
@@ -40,7 +40,7 @@ def joint_train_inversion_model(
     dataset_len = 200
     # Classifier to assign prediction vector and target based on (E)MNIST training data
     temp_model = train_four_layer_mnist_victim(
-        gpus=1
+        gpus=0
     )
 
     def query_mnist(input_data):
